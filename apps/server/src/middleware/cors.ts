9864382e0624df48f9d030299321d3106b9ec29e
Elayaraman <elayaraman007@gmail.com>
2026-03-19 01:16:27 +0530

    removing server folder

--- apps/server/src/middleware/cors.ts
+++
@@ -1,8 +0,0 @@
-import cors from 'cors';
-
-const corsOptions = {
-  origin: 'http://localhost:5173',
-  credentials: true,
-};
-
-export default cors(corsOptions);
--- apps/server/src/socket/index.ts
+++
@@ -1,5 +0,0 @@
-import { Server as HttpServer } from 'http';
-
-export function initSocket(server: HttpServer) {
-  // TODO: Initialize Socket.io
-}
